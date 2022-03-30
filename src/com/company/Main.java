05facd2c9f81b06571e36a837d119a2eeb3e664f
jsvilviv <jsvi.lviv@gmail.com>
2022-03-30 18:55:18 +0300

    тест

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num =10;
+         int num2=5;
+         int num3;
+         num3=num2*num;
+         System.out.println(num3);
+        System.out.print(num3);
+        System.out.println(num3);
+
+    }
+}
