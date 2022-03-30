b7288dc211488ee4089cec56f9cf83bbf8a73be8
jsvilviv <jsvi.lviv@gmail.com>
2022-03-30 19:42:13 +0300

    тест4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int num =10;
-         int num2=5;
-         int num3;
-         num3=num2*num;
-         System.out.println(num3);
-        System.out.print(num3);
-        System.out.println(num3);
+        String name ="Julia";
+       //  int num2=5;
+       //  int num3;
+       //  num3=num2*num;
+
+        System.out.println(name);
+       // System.out.print(num3);
+       // System.out.println(num3);
 
     }
 }
